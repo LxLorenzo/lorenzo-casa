d95bbe2c45a79edff5bea5fe791316f2bec2472c
Lorenzo Casa <lorenzocasa1235@gmail.com>
2023-08-07 02:10:23 -0300

    feat: about section mobile version

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -4,28 +4,36 @@ import React from 'react'
 const About = () => {
   return (
     <section
-      className="container mx-auto h-screen px-4 md:px-8 py-28 lg:p-28 flex flex-col-reverse justify-center items-center gap-5 md:flex-row md:gap-32"
+      className="container mx-auto h-screen px-4 md:px-8 py-28 lg:p-28 flex flex-col items-center md:flex-row md:gap-32"
       id="about"
     >
-      <div className="text-center md:text-left">
-        <h2 className="text-[2rem] md:text-[2.5rem] lg:text-[3rem] xl:text-[4rem] 2xl:text-[5rem font-bold text-primary">
-          Sobre mim
-        </h2>
-        <p className="text-zinc-500 mt-5">
-          Olá! Meu nome é Lorenzo Casa, sou desenvolvedor Front-End React.js e
-          Next.js.
-          <br />
-          Saiba mais sobre mim e meus projetos.
-        </p>
-      </div>
-      <div className="">
+      <h2 className="text-[2rem] md:text-[2.5rem] lg:text-[3rem] xl:text-[4rem] 2xl:text-[5rem] font-bold text-primary">
+        Sobre mim
+      </h2>
+      <div className="flex flex-col mt-12 items-center">
         <Image
           src="/perfil.jpg"
           width={300}
           height={300}
           alt="Perfil"
-          className="rounded-full md:mt-20 xl:mt-32"
+          className="rounded-full border-[3px] border-zinc-950"
         />
+        <div className="text-zinc-500 mt-12 text-justify">
+          <p>
+            Sou um entusiasta da web e sou movido pelo desafio de criar
+            interfaces interativas e responsivas que proporcionem uma
+            experiência excepcional aos usuários. Com um olhar atento ao design
+            e à usabilidade, sempre busco aprimorar minhas habilidades para
+            oferecer soluções modernas e inovadoras.
+          </p>
+          <p>
+            Meu objetivo é continuar crescendo como desenvolvedor front-end,
+            participando de projetos desafiadores e inovadores. Estou em busca
+            de uma primeira oportunidades para contribuir com meu conhecimento
+            técnico e habilidades de comunicação para alcançar resultados
+            excepcionais.
+          </p>
+        </div>
       </div>
     </section>
   )
