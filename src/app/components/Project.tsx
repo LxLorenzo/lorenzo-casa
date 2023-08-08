705a770b773d0aab1b5316d9260d456aba435e08
Lorenzo Casa <lorenzocasa1235@gmail.com>
2023-08-08 17:43:55 -0300

    style: button & project card sizes

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -60,7 +60,7 @@ interface ProjectProps {
 
 const Project = ({ title, description, img, icons }: ProjectProps) => {
   return (
-    <div className="sm:max-w-[46%] flex flex-col border border-zinc-950 rounded-lg">
+    <div className="max-w-[400px] md:max-w-[46%] lg:max-w-[400px] flex flex-col border border-zinc-950 rounded-lg">
       <Image
         src={img}
         width={400}
@@ -69,11 +69,11 @@ const Project = ({ title, description, img, icons }: ProjectProps) => {
         alt="Project"
         className="rounded-t-lg"
       />
-      <div className="flex flex-col p-2">
+      <div className="flex flex-col px-6 py-2 justify-between">
         <h3 className="text-2xl text-primary font-semibold mt-2">{title}</h3>
-        <p className="text-zinc-500 text-sm mb-5">{description}</p>
-        <div className="flex justify-between items-center">
-          <div className="flex gap-2">
+        <p className="text-zinc-500 text-sm">{description}</p>
+        <div className="flex justify-between items-center mt-5">
+          <div className="flex gap-1.5">
             {icons.map((icon, index) => {
               const Icon = iconMap[icon]
               if (!Icon) return null
@@ -86,7 +86,7 @@ const Project = ({ title, description, img, icons }: ProjectProps) => {
               )
             })}
           </div>
-          <Button className="mt-0 py-0">Ver mais</Button>
+          <Button className="mt-0 px-4 py-2 sm:px-4 sm:py-2">Ver mais</Button>
         </div>
       </div>
     </div>
