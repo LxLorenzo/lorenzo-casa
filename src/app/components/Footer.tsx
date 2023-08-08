fdf03d5a8aa0b8af6a91ac9e8d296f12be2820ef
Lorenzo Casa <lorenzocasa1235@gmail.com>
2023-08-08 17:45:23 -0300

    feat: add dynamic project links to footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,26 +1,28 @@
 import Link from 'next/link'
 import React from 'react'
 import { FiExternalLink } from 'react-icons/fi'
+import projectData from '../data/ProjectData'
 
 const Footer = () => {
   return (
     <section className="flex mx-auto flex-col py-14 items-center bg-zinc-950 gap-12 sm:flex-row sm:justify-around">
       <div className="flex flex-col text-center text-zinc-50 text-sm gap-5">
-        <h4 className="text-zinc-500 font-semibold text-2xl">Navegue</h4>
+        <h4 className="text-zinc-50 font-semibold text-2xl">Navegue</h4>
         <Link href="">Início</Link>
         <Link href="#about">Sobre</Link>
         <Link href="#projects">Projetos</Link>
         <Link href="#contacth">Contato</Link>
       </div>
       <div className="flex flex-col text-center text-zinc-50 text-sm gap-5">
-        <h4 className="text-zinc-500 font-semibold text-2xl">Projetos</h4>
-        <Link href="">Projeto 1</Link>
-        <Link href="#about">Projeto 2</Link>
-        <Link href="#projects">Projeto 3</Link>
-        <Link href="#contacth">Projeto 4</Link>
+        <h4 className="text-zinc-50 font-semibold text-2xl">Projetos</h4>
+        {projectData.slice(0, 4).map((project) => (
+          <Link href={`/${project.shortName}`} key={project.id}>
+            {project.title}
+          </Link>
+        ))}
       </div>
       <div className="flex flex-col text-center text-zinc-50 text-sm gap-5">
-        <h4 className="text-zinc-500 font-semibold text-2xl">Contato</h4>
+        <h4 className="text-zinc-50 font-semibold text-2xl">Contato</h4>
         <Link
           href="https://www.linkedin.com/in/lorenzocasa/"
           target="_blank"
