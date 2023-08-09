2cc7dc6968ef9e3c2662b2ad56e715583adf9f6b
Lorenzo Casa <lorenzocasa1235@gmail.com>
2023-08-09 02:05:26 -0300

    style: font size adjust

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
       className="container mx-auto px-2 sm:px-4 md:px-8 lg:px-12 xl:px-16 2xl:px-20 py-28 flex flex-col items-center gap-12"
       id="projects"
     >
-      <h2 className="text-[2rem] md:text-[2.5rem] lg:text-[3rem] xl:text-[4rem] 2xl:text-[4.5rem] font-bold text-primary">
+      <h2 className="text-[2rem] md:text-[2.5rem] lg:text-[3rem] xl:text-[3.5rem] 2xl:text-[4rem] font-bold text-primary">
         Projetos
       </h2>
       <p className="text-zinc-500 text-center">
