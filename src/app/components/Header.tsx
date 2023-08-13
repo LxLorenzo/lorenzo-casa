714315a214a2df08b23ca4e61389da238c5edf24
Lorenzo Casa <lorenzocasa1235@gmail.com>
2023-08-13 20:13:51 -0300

    style: add animated navbar

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -31,58 +31,62 @@ const Header = () => {
       >
         Get in Touch
       </Link>
-      {isMenuOpen ? (
-        <div className="menu z-10 lg:hidden fixed flex flex-col right-0 top-0 w-3/5 h-screen px-4 py-5 rounded-l-lg items-center text-center justify-between">
+      {!isMenuOpen ? (
+        <Image
+          src="/menu.svg"
+          width={28}
+          height={28}
+          alt="Menu"
+          onClick={() => setIsMenuOpen((prev) => !prev)}
+          className="cursor-pointer lg:hidden"
+        />
+      ) : (
+        <div className="fixed right-2 sm:right-8 top-5 z-50">
           <RiCloseFill
             onClick={() => setIsMenuOpen((prev) => !prev)}
             className="self-end cursor-pointer"
             fontSize={28}
           />
-          <nav className="flex flex-col gap-12 pb-[-160px] w-full">
-            <ul className="gap-12 text-zinc-700 font-semibold flex flex-col">
-              <Link
-                className="hover:text-zinc-950 link py-1.5 rounded-md"
-                href="/#about"
-                onClick={() => setIsMenuOpen(false)}
-              >
-                Sobre
-              </Link>
-              <Link
-                className="hover:text-zinc-950 link py-1.5 rounded-md"
-                href="/#projects"
-                onClick={() => setIsMenuOpen(false)}
-              >
-                Projetos
-              </Link>
-              <Link
-                className="hover:text-zinc-950 link py-1.5 rounded-md"
-                href="/#contact"
-                onClick={() => setIsMenuOpen(false)}
-              >
-                Contato
-              </Link>
-            </ul>
-          </nav>
-          <Link
-            href="/#contact"
-            className="2xl:basis-52 text-zinc-700 font-semibold hover:text-zinc-950 mb-40 link w-full py-1.5 rounded-md"
-            onClick={() => setIsMenuOpen(false)}
-          >
-            Get in Touch
-          </Link>
-        </div>
-      ) : (
-        <div className="flex lg:hidden">
-          <Image
-            src="/menu.svg"
-            width={28}
-            height={28}
-            alt="Menu"
-            onClick={() => setIsMenuOpen((prev) => !prev)}
-            className="cursor-pointer"
-          />
         </div>
       )}
+      <div
+        className={`menu lg:hidden fixed flex flex-col right-0 top-0 w-2/3 sm:w-1/2  h-screen rounded-l-lg items-center text-center justify-between ${
+          isMenuOpen ? 'translate-x-0' : 'translate-x-full'
+        } ease-in-out duration-300 z-40`}
+      >
+        <nav className="flex flex-col gap-12 pb-[-160px] w-full fixed top-[30%] px-4">
+          <ul className="gap-12 text-zinc-700 font-semibold flex flex-col">
+            <Link
+              className="hover:text-zinc-950 link py-1.5 rounded-md"
+              href="/#about"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Sobre
+            </Link>
+            <Link
+              className="hover:text-zinc-950 link py-1.5 rounded-md"
+              href="/#projects"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Projetos
+            </Link>
+            <Link
+              className="hover:text-zinc-950 link py-1.5 rounded-md"
+              href="/#contact"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Contato
+            </Link>
+          </ul>
+        </nav>
+        <Link
+          href="/#contact"
+          className="2xl:basis-52 text-zinc-700 font-semibold hover:text-zinc-950 mb-40 link w-full py-1.5 rounded-md absolute bottom-0"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          Get in Touch
+        </Link>
+      </div>
     </header>
   )
 }
