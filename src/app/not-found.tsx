5abb45514ef29e5fc8d07b422a2225892060a8a7
Lorenzo Casa <lorenzocasa1235@gmail.com>
2023-08-15 16:43:24 -0300

    style: not found message

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ const PageNotFound = () => {
   return (
     <div className="container mx-auto min-h-[calc(100vh-500px)] flex flex-col items-center py-28">
       <h1 className="text-4xl font-light text-center text-zinc-950">
-        Oops... Você está perdido?
+        Oops... Parece que você está perdido!
       </h1>
       <Button href="/">Voltar para o início</Button>
     </div>
