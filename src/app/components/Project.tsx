c662799b6fc0ce4b59a927775239c68ff5094f2d
Lorenzo Casa <lorenzocasa1235@gmail.com>
2024-01-05 10:35:28 -0300

    style: ver mais button to the bottom of the card

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -18,14 +18,14 @@ const Project = ({ project }: IProjectCard) => {
         alt="Project"
         className="rounded-t-lg"
       />
-      <div className="flex flex-col px-6 py-2 justify-between">
+      <div className="flex flex-col px-6 py-2 justify-between h-[140px]">
         <h3 className="text-2xl text-primary font-semibold mt-2">
           {project.name}
         </h3>
         <p className="text-zinc-500 text-sm dark:text-zinc-200">
           {project.short_description}
         </p>
-        <div className="flex justify-between items-center mt-5">
+        <div className="flex justify-between items-center mt-auto">
           <div className="flex gap-1.5">
             {project.icons.map((icon, index) => {
               const Icon = iconMap[icon]
