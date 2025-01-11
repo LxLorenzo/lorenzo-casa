abc5578901b661e8776e53cd98b60c1ba0a76486
Lorenzo Casa <90789986+LxLorenzo@users.noreply.github.com>
2025-01-10 22:55:40 -0300

    Update ProfessionalExperience.tsx

--- src/app/components/ProfessionalExperience.tsx
+++ src/app/components/ProfessionalExperience.tsx
@@ -13,9 +13,23 @@ const ProfessionalExperience = () => {
       <div className="flex flex-col gap-5 justify-center items-center">
         <div className="sm:p-6 flex justify-center items-center gap-5 rounded-md hover:bg-zinc-100 transition shadow-md border border-primary p-5 dark:border-zinc-50 dark:hover:bg-neutral-900">
           <div className="flex flex-col gap-1 p-3 w-1/3">
+            <h3 className="text-lg font-semibold text-primary">Desenvolvedor Front-end</h3>
+            <p className="text-sm text-zinc-500 dark:text-zinc-200">Prime Web</p>
+            <p className="text-sm text-zinc-500 dark:text-zinc-200">Jan/2025 - Atual</p>
+          </div>
+          <div className="flex flex-col gap-1 w-2/3">
+            <p className="text-sm text-zinc-600 dark:text-zinc-300">
+              Criação de páginas web com HTML, CSS, JavaScript e PHP. Edição e otimização de imagens para melhor desempenho e estética. Implementação de estratégias de SEO on-page para aumentar a visibilidade nos mecanismos de busca.
+            </p>
+            <div className="flex justify-items items-center gap-2">
+            </div>
+          </div>
+        </div>
+        <div className="sm:p-6 flex justify-center items-center gap-5 rounded-md hover:bg-zinc-100 transition shadow-md border border-primary p-5 dark:border-zinc-50 dark:hover:bg-neutral-900">
+          <div className="flex flex-col gap-1 p-3 w-1/3">
             <h3 className="text-lg font-semibold text-primary">Estágio Front-end</h3>
             <p className="text-sm text-zinc-500 dark:text-zinc-200">Prime Web</p>
-            <p className="text-sm text-zinc-500 dark:text-zinc-200">Out/2023 - Atual</p>
+            <p className="text-sm text-zinc-500 dark:text-zinc-200">Out/2023 - Jan/2025</p>
           </div>
           <div className="flex flex-col gap-1 w-2/3">
             <p className="text-sm text-zinc-600 dark:text-zinc-300">
